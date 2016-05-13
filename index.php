f3f9332db4d28527d37bf0cf5d23a19d9ffc2d73
Ridvan Baluyos <ridvan@baluyos.net>
2016-05-13 15:36:12 +0800

    auto refresh every hour

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html class="no-js" lang="en">
 	<head>
 	<meta charset="utf-8" />
+	<meta http-equiv="refresh" content="3600">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
 	<title>PHVote: General Elections 2016</title>
 	<meta name="robots" content="index, follow">
